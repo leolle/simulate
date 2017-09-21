7f543993885265ba7fe3e0f904540b8df81df052
leolle <victor.wuv@gmail.com>
2017-09-21 16:26:36 +0800

    test roll weight using 3 crude oil future contracts.

--- backtest/test_future.py
+++ backtest/test_future.py
@@ -6,6 +6,7 @@ from lib.gftTools import gsConst, gftIO, gsUtils
 from datetime import datetime
 import sys
 import itertools
+import quandl
 
 """
 1) 策略初始化函数
@@ -75,3 +76,94 @@ position_data = np.array([[1, 1, 1, 1, 1, 1],
                  [np.nan, np.nan, np.nan, np.nan, 1, 1]])
 
 position = pd.DataFrame(position_data.T, index=dates, columns=index)
+
+wti_near = pd.read_csv('CLF2014.csv', index_col='Date')
+wti_far = pd.read_csv('CLG2014.csv', index_col='Date')
+# wti_near = quandl.get("CME/CLF2014", authtoken="G-_-G7yn75tX64T3fKXc")
+wti_far_far = pd.read_csv('CLH2014.csv', index_col='Date')
+# wti_far = quandl.get("CME/CLG2014", authtoken="G-_-G7yn75tX64T3fKXc")
+wti = pd.DataFrame({'CLF2014': wti_near['Settle'],
+                    'CLG2014': wti_far['Settle']}, index=wti_far.index)
+
+start_date = wti_near.index[0]
+expiry_dates = pd.Series({'CLF2014': datetime(2013, 12, 19),
+                          'CLG2014': datetime(2014, 1, 21)}).sort_values()
+rollover_days = 5
+"""This constructs a pandas DataFrame that contains weights (between 0.0 and 1.0)
+of contract positions to hold in order to carry out a rollover of rollover_days
+prior to the expiration of the earliest contract. The matrix can then be
+'multiplied' with another DataFrame containing the settle prices of each
+contract in order to produce a continuous time series futures contract."""
+contracts = wti.columns
+# Construct a sequence of dates beginning from the earliest contract start
+# date to the end date of the final contract
+# dates = pd.date_range(start_date, expiry_dates[-1], freq='B')
+dates = wti_near.index.append(wti_far.index.append(wti_far_far.index))
+# Create the 'roll weights' DataFrame that will store the multipliers for
+# each contract (between 0.0 and 1.0)
+roll_weights = pd.DataFrame(np.zeros((len(dates), len(contracts))),
+                            index=dates, columns=contracts)
+prev_date = roll_weights.index[0]
+
+# Loop through each contract and create the specific weightings for
+# each contract depending upon the settlement date and rollover_days
+for i, (item, ex_date) in enumerate(expiry_dates.iteritems()):
+    print(i, item, ex_date)
+    if i < len(expiry_dates) - 1:
+        roll_weights.ix[prev_date:ex_date - pd.offsets.BDay(), item] = 1
+        roll_rng = pd.date_range(end=ex_date - pd.offsets.BDay(),
+                                 periods=rollover_days + 1, freq='B')
+
+        # Create a sequence of roll weights (i.e. [0.0,0.2,...,0.8,1.0]
+        # and use these to adjust the weightings of each future
+        decay_weights = np.linspace(0, 1, rollover_days + 1)
+        roll_weights.ix[roll_rng, item] = 1 - decay_weights
+        roll_weights.ix[roll_rng, expiry_dates.index[i+1]] = decay_weights
+    else:
+        roll_weights.ix[prev_date:, item] = 1
+    prev_date = ex_date
+
+# Construct the continuous future of the WTI CL contracts
+wti_cts = (wti * roll_weights).sum(1).dropna()
+
+
+def futures_rollover_weights(start_date, expiry_dates, contracts, rollover_days=5):
+    """This constructs a pandas DataFrame that contains weights (between 0.0 and 1.0)
+    of contract positions to hold in order to carry out a rollover of rollover_days
+    prior to the expiration of the earliest contract. The matrix can then be
+    'multiplied' with another DataFrame containing the settle prices of each
+    contract in order to produce a continuous time series futures contract."""
+
+    # Construct a sequence of dates beginning from the earliest contract start
+    # date to the end date of the final contract
+    dates = pd.date_range(start_date, expiry_dates[-1], freq='B')
+
+    # Create the 'roll weights' DataFrame that will store the multipliers for
+    # each contract (between 0.0 and 1.0)
+    roll_weights = pd.DataFrame(np.zeros((len(dates), len(contracts))),
+                                index=dates, columns=contracts)
+    prev_date = roll_weights.index[0]
+
+    # Loop through each contract and create the specific weightings for
+    # each contract depending upon the settlement date and rollover_days
+    for i, (item, ex_date) in enumerate(expiry_dates.iteritems()):
+        if i < len(expiry_dates) - 1:
+            roll_weights.ix[prev_date:ex_date - pd.offsets.BDay(), item] = 1
+            roll_rng = pd.date_range(end=ex_date - pd.offsets.BDay(),
+                                     periods=rollover_days + 1, freq='B')
+
+            # Create a sequence of roll weights (i.e. [0.0,0.2,...,0.8,1.0]
+            # and use these to adjust the weightings of each future
+            decay_weights = np.linspace(0, 1, rollover_days + 1)
+            roll_weights.ix[roll_rng, item] = 1 - decay_weights
+            roll_weights.ix[roll_rng, expiry_dates.index[i+1]] = decay_weights
+        else:
+            roll_weights.ix[prev_date:, item] = 1
+        prev_date = ex_date
+    return roll_weights
+
+
+weights = futures_rollover_weights(wti_near.index[0], expiry_dates, wti.columns)
+# Construct the continuous future of the WTI CL contracts
+wti_cts = (wti * weights).sum(1).dropna()
+
