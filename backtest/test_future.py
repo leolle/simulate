dab65ba29d6ad50ab370f5e625a1b236503edce5
leolle <victor.wuv@gmail.com>
2017-09-30 09:34:15 +0800

    update

--- backtest/test_future.py
+++ backtest/test_future.py
@@ -55,39 +55,38 @@ name = {
 data.rename(columns=lambda x: name[x], inplace=True)
 target = data['contract_name'].unique()
 
-    roll_position = pd.DataFrame()
-    for contract in target:
-        contract_data = data[data['contract_name'] == contract]
-        # contract_data.set_index('date', inplace=True)
-        contract_expiry_dates = contract_data[['contract_code', 'settlement_date']].\
-                                drop_duplicates().sort_values('settlement_date')
-        contract_expiry_dates.set_index('contract_code', inplace=True)
-        contract_expiry_dates = contract_expiry_dates[
-            contract_expiry_dates.columns[0]]
-        contract_data = contract_data.loc[:, [
-            'date', 'contract_code', 'close_price'
-        ]]
-        contract_data = contract_data.pivot(
-            index='date', columns='contract_code', values='close_price')
-        contracts = contract_data.columns
-        contract_start_date = contract_data.index[0]
-        contract_dates = contract_data.index
+roll_position = pd.DataFrame()
+for contract in target:
+    contract_data = data[data['contract_name'] == contract]
+    # contract_data.set_index('date', inplace=True)
+    contract_expiry_dates = contract_data[['contract_code', 'settlement_date']].\
+                            drop_duplicates().sort_values('settlement_date')
+    contract_expiry_dates.set_index('contract_code', inplace=True)
+    contract_expiry_dates = contract_expiry_dates[contract_expiry_dates.columns[
+        0]]
+    contract_data = contract_data.loc[:,
+                                      ['date', 'contract_code', 'close_price']]
+    contract_data = contract_data.pivot(
+        index='date', columns='contract_code', values='close_price')
+    contracts = contract_data.columns
+    contract_start_date = contract_data.index[0]
+    contract_dates = contract_data.index
 
-        contract_roll_position = pd.DataFrame(
-            np.zeros((len(contract_dates), len(contracts))),
-            index=contract_dates,
-            columns=contracts)
-        prev_date = contract_roll_position.index[0]
-        # Loop through each contract and create the specific position for
-        # each contract depending upon the settlement date and rollover_days
-        for i, (item, ex_date) in enumerate(contract_expiry_dates.iteritems()):
-            # print(i, item, ex_date)
-            if i < len(contract_expiry_dates) - 1:
-                idx_pre_ex_date = contract_data.index.searchsorted(ex_date)
-                pre_ex_date = contract_dates[idx_pre_ex_date - 1]
-                contract_roll_position.ix[prev_date:pre_ex_date, item] = 1
-            else:
-                contract_roll_position.ix[prev_date:, item] = 1
-                prev_date = ex_date
-                roll_position = pd.concat(
-                    [roll_position, contract_roll_position], axis=1)
+    contract_roll_position = pd.DataFrame(
+        np.zeros((len(contract_dates), len(contracts))),
+        index=contract_dates,
+        columns=contracts)
+    prev_date = contract_roll_position.index[0]
+    # Loop through each contract and create the specific position for
+    # each contract depending upon the settlement date and rollover_days
+    for i, (item, ex_date) in enumerate(contract_expiry_dates.iteritems()):
+        # print(i, item, ex_date)
+        if i < len(contract_expiry_dates) - 1:
+            idx_pre_ex_date = contract_data.index.searchsorted(ex_date)
+            pre_ex_date = contract_dates[idx_pre_ex_date - 1]
+            contract_roll_position.ix[prev_date:pre_ex_date, item] = 1
+        else:
+            contract_roll_position.ix[prev_date:, item] = 1
+            prev_date = ex_date
+            roll_position = pd.concat(
+                [roll_position, contract_roll_position], axis=1)
