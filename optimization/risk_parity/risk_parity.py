5cdf556be1237d28146f6b57b7e1e9a8b4bdf4da
leolle <victor.wuv@gmail.com>
2018-08-25 15:17:47 +0800

    add gs python lib

--- optimization/risk_parity/risk_parity.py
+++ optimization/risk_parity/risk_parity.py
@@ -10,6 +10,29 @@ import matplotlib.pyplot as plt
 from IPython.display import Image, display
 from scipy.optimize import minimize
 import time
+import logging
+logging.basicConfig(
+    format='%(asctime)s : %(levelname)s : %(message)s', level=logging.DEBUG)
 
-data = pd.read_csv('./assets.csv', index_col='Date', parse_dates=True).rename(columns={"CBA00332.CS":"Bond","NH0100.NHF":"Commodity","000300.SH":"Equity"})
+data = pd.read_csv(
+    './assets.csv', index_col='Date', parse_dates=True).rename(
+        columns={
+            "CBA00332.CS": "Bond",
+            "NH0100.NHF": "Commodity",
+            "000300.SH": "Equity"
+        })
 data.head()
+tickers = ['Bond', 'Commodity', 'Equity']
+
+lookback = 21 * 6
+corr_lookback = 21 * 24
+periodicity = 252
+
+leverage = 2
+margin_rate = .02768
+
+n_tickers = len(tickers)
+N = len(data)
+
+df_returns = data[tickers].pct_change().dropna()
+logging.debug(df_returns.head())
